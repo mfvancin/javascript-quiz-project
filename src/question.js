e8007a3a68d8a21b1b1f16da7d611b8ed1c4c391
Manuele Vancin <mfvancin@gmail.com>
2024-04-15 17:31:47 +0100

    MiniProjectDay1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+      this.text = text;
+      this.choices = choices;
+      this.answer = answer;
+      this.difficulty = difficulty;
+    };
 
-    // 2. shuffleChoices()
-}
+    shuffleChoices() {
+      for (let i = this.choices.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+        [this.choices[i], this.choices[randomIndex]] = [
+          this.choices[randomIndex],
+          this.choices[i],
+        ];
+      };
+    };
+  };
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,48 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion () {
+        return this.questions[this.currentQuestionIndex]; 
+    };
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion () {
+        return this.currentQuestionIndex ++;
+    };
 
-    // 4. shuffleQuestions()
+    shuffleQuestions () {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            let randomIndex = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[randomIndex]] = [
+            this.questions[randomIndex],
+            this.questions[i],
+            ];
+        };
+    };
 
-    // 5. checkAnswer(answer)
+    checkAnswer (answer) {
+        return this.correctAnswers ++;
+    };
 
-    // 6. hasEnded()
-}
+    hasEnded () {
+        if (this.currentQuestionIndex === this.questions.length){
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+    filterQuestionsByDifficulty (difficulty) {
+        
+    };
+
+    averageDifficulty () {
+
+    };
+};
